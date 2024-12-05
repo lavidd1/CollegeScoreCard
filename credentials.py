e6443d3557ee73e8c3554c971116bd1bc55f07bb
David Li <davidliwashignton24@gmail.com>
2024-12-05 14:08:42 -0500

    Added local changes

--- credentials.py
+++ credentials.py
@@ -1,6 +1,4 @@
-# TODO
-
 DB_HOST = "pinniped.postgres.database.azure.com"
-DB_NAME = "abradsha"
-DB_USER = "abradsha"
-DB_PASSWORD = "hcVjAJ5zo3"
+DB_NAME = "dcli"
+DB_USER = "dcli"
+DB_PASSWORD = "$agyoZz7W6"
